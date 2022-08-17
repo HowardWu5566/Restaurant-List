4dec4e7d5c8e53563625230f8564a85cfbc6c3fb
HowardWu5566 <ggg1237890@gmail.com>
2022-08-17 14:33:46 +0800

    Add two functions: search by rating and search by location

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // 載入express
+const { query } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -17,9 +18,18 @@ app.set('view engine', 'handlebars')
 // 靜態檔案路徑
 app.use(express.static('public'))
 
+// 產生 district 陣列，用來動態產生下拉式選單
+const districts = ['地區']
+restaurantList.results.forEach(restaurant => {
+  const address = restaurant.location
+  const district = address.slice(address.indexOf('市') + 1, address.indexOf('區') + 1)
+  if (!districts.includes(district))
+    districts.push(district)
+})
+
 // 設定路由 - index 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList.results, districts })
 })
 
 // 設定路由 - show
@@ -30,12 +40,30 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 
 // 設定路由 - search
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return (restaurant.name.toLowerCase() + restaurant.category).includes(keyword)
-  })
-
-  res.render('index', { restaurants })
+  let restaurants
+  const type = req.query.type
+  const district = req.query.district
+  // 根據餐廳或分類篩選
+  if (type === '餐廳、分類') {
+    const keyword = req.query.keyword.toLowerCase()
+    restaurants = restaurantList.results.filter(restaurant => {
+      return ((restaurant.name.toLowerCase() + restaurant.category).includes(keyword))
+    })
+    // 根據評分篩選
+  } else if (type === '評分') {
+    const score = Number(req.query.keyword)
+    restaurants = restaurantList.results.filter(restaurant => {
+      return (restaurant.rating >= score)
+    })
+  }
+  // 根據地區篩選
+  if (district !== '地區') {
+    restaurants = restaurants.filter(restaurant => {
+      return (restaurant.location.includes(district))
+    })
+  }
+  // 渲染篩選結果
+  res.render('index', { restaurants, districts })
 })
 
 // 啟動伺服器

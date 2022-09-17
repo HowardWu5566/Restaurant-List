80213038a9fdb87c3d489f538ddf88bb949cfd65
HowardWu5566 <ggg1237890@gmail.com>
2022-09-17 15:30:20 +0800

    feat: add login page

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 // 路由符合就導向 home, restaurants 或search 模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
